3123a9fd6832d1ed4b1ab52cbd72d8438373fade
sneha-rajshekar03 <rajshekarsnehaa@gmail.com>
2025-10-05 06:31:37 +0530

    Update ProfilePopupManager.jsx

--- src/app/components/ProfilePopupManager/ProfilePopupManager.jsx
+++ src/app/components/ProfilePopupManager/ProfilePopupManager.jsx
@@ -15,8 +15,8 @@ import {
   MapPin,
 } from "lucide-react";
 
-const POPUP_INTERVAL_MS = 4000;
-const POPUP_INITIAL_DELAY_MS = 2000;
+const POPUP_INTERVAL_MS = 70000;
+const POPUP_INITIAL_DELAY_MS = 20000;
 
 const iconMap = {
   User,
